af4187eb00a04d6702fa197ae6622c6a5a04b01d
Davit Manaseryan <davidmanaseryan03w@gmail.com>
2026-02-09 12:47:18 -0800

    Redirect user from /auth pages if already logged in.

--- src/app/auth/[path]/page.tsx
+++ src/app/auth/[path]/page.tsx
@@ -1,4 +1,6 @@
 import { AuthView } from "@neondatabase/auth/react";
+import { auth } from "@/lib/auth/server";
+import { redirect } from "next/navigation";
 
 export const dynamicParams = false;
 
@@ -8,6 +10,11 @@ export default async function AuthPage({
   params: Promise<{ path: string }>;
 }) {
   const { path } = await params;
+  const { data: session } = await auth.getSession();
+
+  if (session && path != "sign-out") {
+    redirect("/");
+  }
 
   return (
     <main className="container mx-auto flex grow flex-col items-center justify-center gap-3 self-center p-4 md:p-6">
