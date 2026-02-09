aade9cb48d055f16c52fb8fe0d86833652eb235e
Davit Manaseryan <davidmanaseryan03w@gmail.com>
2026-02-09 12:46:24 -0800

    Added analytics and speed-insights from vercel.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,10 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/navbar";
 import { authClient } from "@/lib/auth/client";
-import { NeonAuthUIProvider, UserButton } from "@neondatabase/auth/react";
+import { NeonAuthUIProvider } from "@neondatabase/auth/react";
 import { SyncTimezone } from "@/components/sync-timezone";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,7 +39,7 @@ export default function RootLayout({
           redirectTo="/"
           emailOTP
           social={{ providers: ["google"] }}
-          defaultTheme="light"
+          // defaultTheme="light"
         >
           <div className="fixed inset-0 -z-10 bg-linear-to-br from-slate-50 via-sky-50 to-indigo-50 dark:from-slate-950 dark:via-slate-900 dark:to-indigo-950" />
           <div className="fixed inset-0 -z-10 opacity-30 [background:radial-gradient(circle_at_20%_20%,rgb(152, 14, 14),transparent_45%),radial-gradient(circle_at_80%_30%,rgba(0, 4, 255, 0.3),transparent_40%),radial-gradient(circle_at_50%_80%,rgba(236,72,153,0.18),transparent_45%)]" />
@@ -47,6 +49,8 @@ export default function RootLayout({
           </header>
           {children}
         </NeonAuthUIProvider>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
