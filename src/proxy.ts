9a5802133a9b4e2043a907014c46935c8c814ec1
Davit Manaseryan <davidmanaseryan03w@gmail.com>
2026-02-13 01:01:57 -0800

    temp fix attempt #2

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,6 +1,5 @@
 // src/proxy.ts
 import { auth } from "@/lib/auth/server";
-import { headers } from "next/headers";
 import { NextResponse, type NextRequest } from "next/server";
 
 /**
@@ -9,24 +8,23 @@ import { NextResponse, type NextRequest } from "next/server";
  */
 
 export default async function proxy(request: NextRequest) {
-  const { pathname } = request.nextUrl;
+  // const { pathname } = request.nextUrl;
 
   // 1. Ensure dynamic context by calling headers()
   // This helps auth.getSession() retrieve the __Secure-neon-auth.session_token
-  await headers();
-  const { data: session } = await auth.getSession();
+  // const { data: session } = await auth.getSession();
 
-  console.log("SESSION:", session);
-  console.log("PATHNAME:", pathname);
+  // console.log("SESSION:", session);
+  // console.log("PATHNAME:", pathname);
 
-  // 2. REDIRECT AUTHENTICATED USERS: If logged in and hitting an auth page
-  if (
-    session &&
-    pathname.startsWith("/auth") &&
-    !pathname.includes("sign-out")
-  ) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
+  // // 2. REDIRECT AUTHENTICATED USERS: If logged in and hitting an auth page
+  // if (
+  //   session &&
+  //   pathname.startsWith("/auth") &&
+  //   !pathname.includes("sign-out")
+  // ) {
+  //   return NextResponse.redirect(new URL("/", request.url));
+  // }
 
   // 3. PROTECT ROUTES: Call the standard Neon middleware for protected paths
   return auth.middleware({
